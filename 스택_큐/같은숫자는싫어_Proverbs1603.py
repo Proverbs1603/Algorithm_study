a2e90124dc7baa76acc0640398ff2c17589e23fe
김신웅 <110598358+Proverbs1603@users.noreply.github.com>
2024-03-08 11:53:54 +0900

    Add files via upload
    2024-03-08 프로그래머스 스택/큐

---
+++ 스택_큐/같은숫자는싫어_Proverbs1603.py
@@ -0,0 +1,12 @@
+def solution(arr):
+
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+            continue;
+            
+        if arr[i-1] != arr[i]:
+            answer.append(arr[i])
+
+    return answer
