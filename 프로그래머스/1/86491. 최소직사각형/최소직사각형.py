0b9d78e49c7b56ff17f64fe37791ee5e37cdc4d9
김신웅 <110598358+Proverbs1603@users.noreply.github.com>
2024-03-13 18:41:17 +0900

    [level 1] Title: 최소직사각형, Time: 2.22 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -3,19 +3,19 @@ def solution(sizes):
     #제일 큰 애랑 그 size에서 큰 애를 뽑아서 비교하고 
     #작은애들끼리 비교해서 가장 큰 애를 고르면 됨.
     max_size = 0
+    max_size_in_small_num = 0
     #가장큰 수 구하기
     for size in sizes:
         if size[0] >= max_size:
             max_size = size[0]
         if size[1] >= max_size:
             max_size = size[1]
-    
-    max_size_in_small_num = 0
-    #작은 수들끼리 비교해서 제일 큰 애 구하기    
-    for size in sizes:
+            
         internal_min_num = min(size)
         if internal_min_num >= max_size_in_small_num:
             max_size_in_small_num = internal_min_num
+
+    #작은 수들끼리 비교해서 제일 큰 애 구하기    
 
         
     answer = max_size * max_size_in_small_num 
